5a2c2be7956787cf37670aa22b7d5cc388854f18
LukeSchaffel <lukeschaffel@gmail.com>
2022-10-26 14:40:14 -0400

    table and select user

--- app.js
+++ app.js
@@ -2,37 +2,72 @@
 // when a user is selected, display their posts
 
 const BASE_URL = 'https://jsonplaceholder.typicode.com'
-const table = document.getElementById('table')
+
+let state = 'table'
+let selectedUser 
 
 
+const selectUser = (user) => {
+  selectedUser = user
+  console.log(user, selectedUser);
+}
+
+console.log(selectedUser);
 
 const createTable = async () => {
   const res = await fetch(`${BASE_URL}/users`)
   const json = await res.json()
-  console.log(json);
+
+  const table = document.createElement('table')
+  const header = document.createElement('tr')
+
+  const headers = ['Name', 'Company', 'City', 'ID', 'Posts']
+
+  headers.forEach((item)=> {
+    const cell = document.createElement('th')
+    const cellText = document.createTextNode(item)
+    cell.appendChild(cellText)
+    table.appendChild(cell)
+
+  })
 
   json.forEach((user, i) => {
     const row = document.createElement("tr")
-
     const name = user.name
     const company = user.company.name
     const city = user.address.city
     const id = user.id
     
-    const data = [ name, company, city, id]
+    const data = [ name, company, city, id, 'posts']
 
     data.forEach((obj, j) => {
-      const cell = document.createElement('td')
-      const text = document.createTextNode(obj) 
-      cell.appendChild(text)
-      row.appendChild(cell)
-      table.appendChild(row)
+      if (obj !== 'posts'){
+        const cell = document.createElement('td')
+        const text = document.createTextNode(obj) 
+        cell.appendChild(text)
+        row.appendChild(cell)
+        table.appendChild(row)
+      } else {
+        const cell = document.createElement('td')
+        const btn = document.createElement('button')
+        btn.innerHTML = 'SHOW'
+        btn.addEventListener('click', ()=> {
+          selectUser(user)
+        })
+        cell.appendChild(btn)
+        row.appendChild(cell)
+        table.appendChild(row)
+      }
 
     })
   })
 
-
-
+  document.body.appendChild(table)
+  table.setAttribute('border', '2')
 }
 
-createTable()
+
+if(state === 'table') {
+  createTable()
+
+}
